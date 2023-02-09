7c1879e5cff2fbd6090237dfac96922ee302e3ed
yyueun <sw3623@ajou.ac.kr>
2023-02-09 11:50:31 +0900

    1.할일 목록 리스팅

---
+++ my-todo1/src/components/TodoMain.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="page">
+    <header><h1>Vue Fire todo1</h1></header>
+    <main>
+      <div class="todos">
+        <div class="write">
+          <input type="text"/>
+          <button class="btn add">Add</button>
+        </div>
+        <ul class="list">
+          <li v-for ="(todo,i) in todos">
+            <i   class="far fa-check-square"></i>
+            <!-- done일때 fas, yet일 때 far -->
+            <span>
+              {{ todo.text }}
+              <b>
+                <a href="">Edit</a>
+                <a href="">Del</a>
+              </b>
+            </span>
+          </li>
+        </ul>
+      </div>
+    </main>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+       return{
+
+        todos:[
+        {text: '공부하기', state: 'yet'},
+        {text: '운동하기', state: 'done'},
+        {text: '글쓰기', state: 'done'},
+      ]}
+
+    },
+    watch:{
+       
+
+        }
+    }
+
+
+</script>
+
+<style>
+
+</style>
