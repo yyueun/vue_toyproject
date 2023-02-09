4fa4727c5c63c170ec772198d992dfdbef1ad69c
yyueun <sw3623@ajou.ac.kr>
2023-02-09 14:52:26 +0900

    4.삭제기능

--- my-todo1/src/components/TodoMain.vue
+++ my-todo1/src/components/TodoMain.vue
@@ -2,14 +2,24 @@
     <div class="page">
     <header><h1>Vue Fire todo1</h1></header>
     <main>
-      <div class="todos">
-        <div class="write">
+    <div class="todos">
+        <!-- 등록 -->
+        <div class="write" v-if="writeState ==='add'">
           <input
           ref="writeArea"  
           type="text"   
           v-model="addItemText" 
           @keyup.enter="addItem"/>
           <button class="btn add" @click="addItem()" >Add</button>
+        </div>
+        <!-- 수정 -->
+        <div class="write" v-else>
+          <input
+          ref="writeArea"  
+          type="text"   
+          v-model="editItemText" 
+          @keyup.enter="editSave"/>
+          <button class="btn add" @click="editSave" >Save</button>
         </div>
         <ul class="list">
           <li v-for ="(todo, i) in todos" :key="i">
@@ -20,8 +30,8 @@
             <span>
               {{ todo.text }}
               <b>
-                <a href="">Edit</a>
-                <a href="">Del</a>
+                <a  href=""  @click.prevent="editShow(i)">Edit</a>
+                <a href="" @click.prevent="delitem(i)">Del</a>
               </b>
             </span>
           </li>
@@ -29,13 +39,17 @@
       </div>
     </main>
     </div>
+    
 </template>
 
 <script>
 export default {
     data(){
        return{
+        writeState:'add',
         addItemText:'',//인풋태그와 연결
+        ediItemText:'',
+        crrEditItem:'', //몇번째꺼를 수정하는지 글의 인덱스 저장
 
         todos:[
         {text: '공부하기', state: 'yet'},
@@ -52,8 +66,31 @@ export default {
             this.addItemText='';
             
             },
+
+            //체크박스 선택,취소 
         checkItem(i){
-            this.todos[i].state = 'done'
+            if(this.todos[i].state === 'yet'){
+                this.todos[i].state = 'done'
+            }else{
+                this.todos[i].state ='yet'
+            }
+
+        },
+        editShow(index){
+            this.crrEditItem = index
+            this.writeState = 'edit'
+            this.editItemText = this.todos[index].text  //수정할 글 내용 인풋창에 보여주기
+        },
+        editSave(){
+            this.todos[this.crrEditItem].text = this.editItemText
+            this.writeState ="add"
+            //add로 변경
+
+
+        },
+        delitem(i){
+            this.todos.splice(i,1)
+
 
         }
         },
